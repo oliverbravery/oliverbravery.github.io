de6bf64db1daad0775ca0f777ef8f41ecc6e7db4
Oli-JB <107476394+oliverbravery@users.noreply.github.com>
2022-06-15 22:40:45 +0100

    Dynamic projects from json file

--- docs/script.js
+++ docs/script.js
@@ -29,7 +29,8 @@ function GetTagColour(tagName) {
 }
 
 function MakeProjects() {
-    for(let i = 0; i<1; ++i) {
+    var details = GetJSONFile("projects.json");
+    details.forEach(e => {
         var div = document.createElement('div');
         div.classList = "max-w-md mx-auto bg-gray-900 rounded-xl shadow-xl overflow-hidden md:max-w-2x1 m-4";
         var div2 = document.createElement('div');
@@ -41,18 +42,18 @@ function MakeProjects() {
         var h1 = document.createElement("h1");
         h1.classList = "h-12 text-3xl text-gray-300 font-semibold";
         h1.id = "projName";
-        h1.innerText = "Hello, World!!!!";
+        h1.innerText = `${e["title"]}`;
         div3.appendChild(h1);
         var div4 = document.createElement("div");
-        c = GetTagColour("mod");
+        c = GetTagColour(`${e["tag"]}`);
         div4.classList = `uppercase tracking-wide text-sm text-${c} font-semibold`;
         div4.id = "projTag";
-        div4.innerText = "mod";
+        div4.innerText = `${e["tag"]}`;
         div3.appendChild(div4);
         var p = document.createElement("p");
         p.classList = "mt-2 text-gray-400";
         p.id = "projDesc";
-        p.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate quia itaque, iste perferendis quasi molestias exercitationem omnis. Accusantium voluptatibus repellendus, itaque delectus sequi quae et, voluptates blanditiis doloremque consectetur qui?";
+        p.innerText = `${e["desc"]}`;
         div3.appendChild(p);
         var div5 = document.createElement("div");
         div5.classList = "flex items-center";
@@ -60,8 +61,10 @@ function MakeProjects() {
         var button = document.createElement("button");
         button.classList = "mt-5 text-center flex-auto shadow-md max-w-md mx-auto h-8 hover:bg-slate-700 active:bg-slate-900 focus:outline-none bg-slate-800 text-white rounded-xl px-3 w-60";
         button.innerText = "View more";
+        button.onclick = function() {
+            location.href = `${e["loc"]}`;
+        };
         div5.appendChild(button);
-
         document.getElementById("projGrid").appendChild(div);
-    }
+    });
 }
